5f5e4970ac5deb5809466cb5dc78e07281b11bc0
shocktherap <moccaify@gmail.com>
2014-09-16 08:50:13 +0700

    ccategory crud and routes

---
+++ app/controllers/backend/category_controller.rb
@@ -0,0 +1,60 @@
+module Backend
+  class CategoryController < Backend::ApplicationController
+    before_action :category, except: [:index]
+
+    def index
+      @categories = Product::Category.page(params[:page])
+    end
+
+    def new
+    end
+
+    def create
+      if @category.update_attributes category_params
+        flash[:success] = 'Category Created'
+        redirect_to backend_category_index_path
+      else
+        render action: :new
+      end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+      if @category.update_attributes category_params
+        flash[:success] = 'Category Updated'
+        redirect_to backend_category_index_path
+      else
+        render action: :edit
+      end
+    end
+
+    def destroy
+      if @category.destroy
+        flash[:success] = 'Category Destroyed'
+      else
+        flash[:error] = 'Category Failed to destroy'
+      end
+
+      redirect_to backend_category_index_path
+    end
+
+    private
+
+    def category
+      @category ||= if params[:id]
+                      Product::Category.find(params[:id])
+                    else
+                      Product::Category.new
+                    end
+    end
+
+    def category_params
+      params.require(:product_category).permit(:name)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to:  'home#index'
+
+  namespace :backend do
+    get '/', to: 'home#index'
+    resources :category
+  end
 end
