58f795e6a42970db46a56ee70d30db3da5c48b79
shocktherap <moccaify@gmail.com>
2014-09-22 09:15:54 +0700

    thank devi as contributor shadow

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -1,5 +1,7 @@
 /*
 //= require jquery
+//= require jquery_ujs
+//= require jquery-ui
 //= require jquery-1.8.3.min
 //= require bootstrap.min
 //= require jquery.dcjqaccordion.2.7
@@ -7,5 +9,9 @@
 //= require jquery.nicescroll
 //= require jquery.sparkline
 //= require common-scripts
-//= require jquery_ujs
+//= require bootstrap-select.min
+//= require_self
 */
+$(document).ready(function() {
+  $('.selectpicker').selectpicker();
+});
