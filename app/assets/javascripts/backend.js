186a44d2162412f0d73ed76f380a34cebe3cc731
shocktherap <moccaify@gmail.com>
2014-09-22 07:00:32 +0700

    update backend

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -1,19 +1,9 @@
 /*
 //= require jquery
 //= require jquery-1.8.3.min
-//= require jquery-2.1.1
-//= require jquery-ui-1.9.2.custom.min
-//= require jquery_ujs
-//= require twitter/bootstrap
-//= require bootstrap-switch
 //= require jquery.dcjqaccordion.2.7
-//= require jquery.nicescroll
 //= require jquery.scrollTo.min
-//= require common-scripts
-//= require jquery.backstretch.min
+//= require jquery.nicescroll
 //= require jquery.sparkline
-//= require jquery.tagsinput
-//= require sparkline-chart
-//= require tasks
-//= require turbolinks
+//= require common-scripts
 */
